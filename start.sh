3783446150bf2ab66b8c3683ce4da55b8d64cac9
adjarar <82608399+adjarar@users.noreply.github.com>
2023-04-25 17:26:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-echo "Container Started"
-export PYTHONUNBUFFERED=1
 
-if [[ $PUBLIC_KEY ]]
-then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-    cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
-    chmod 700 -R ~/.ssh
-    cd /
-    service ssh start
-    echo "SSH Service Started"
-fi
-
-sleep infinity
+su - webui -c "download_models.sh"
