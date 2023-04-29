cc6f975cb2fdec4269b65fa055c7421a13318f4f
adjarar <82608399+adjarar@users.noreply.github.com>
2023-04-29 15:03:23 +0200

    Update on_docker_start.sh

--- on_docker_start.sh
+++ on_docker_start.sh
@@ -1,5 +1,5 @@
-cd /home/webui
-touch hello_world
+cd ~/stable-diffusion-webui
+wget https://raw.githubusercontent.com/adjarar/webui-docker-files/main/webui-user.sh -O webui-user.sh
 
 cd ~/stable-diffusion-webui/models/Stable-diffusion
 gdown 15zgfkYbrPjLA2j69VSiE_ITVRRFDF9I7 # owlstudio model
@@ -11,3 +11,5 @@ git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git
 
 cd ~/stable-diffusion-webui/extensions/sd-webui-controlnet/models
 wget https://huggingface.co/kohya-ss/ControlNet-diff-modules/resolve/main/diff_control_sd15_openpose_fp16.safetensors
+wget https://huggingface.co/kohya-ss/ControlNet-diff-modules/resolve/main/diff_control_sd15_canny_fp16.safetensors
+wget https://huggingface.co/kohya-ss/ControlNet-diff-modules/resolve/main/diff_control_sd15_depth_fp16.safetensors
