87e8033d8ee69914e326f881c0f0bf0b50b224e0
adjarar <82608399+adjarar@users.noreply.github.com>
2023-05-07 14:54:48 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/adjarar/webui-docker-files/main/on_docker_start.sh
+wget https://raw.githubusercontent.com/adjarar/webui-docker-files/latest/on_docker_start.sh
 chmod +x on_docker_start.sh
 mv on_docker_start.sh /home/webui/
 
