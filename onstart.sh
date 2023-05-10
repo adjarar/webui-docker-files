1baf56eb3cb0b74715afcaa548e08fadf52e80a5
adjarar <82608399+adjarar@users.noreply.github.com>
2023-05-11 01:02:27 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -2,7 +2,7 @@
 
 wget https://raw.githubusercontent.com/adjarar/webui-docker-files/latest/on_docker_start.sh
 chmod +x on_docker_start.sh
-mv on_docker_start.sh /home/webui/
+mv on_docker_start.sh /usr/local/bin
 
 # Switch to the webui user
 su -s /bin/bash webui << 'EOF'
