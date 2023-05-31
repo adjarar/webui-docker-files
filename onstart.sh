c5a3cbb26112655c825874396da07b83c1a35925
adjarar <82608399+adjarar@users.noreply.github.com>
2023-05-31 06:24:06 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -3,8 +3,8 @@
 sudo -u webui bash << 'EOF'
 
 cd ~
-git clone https://github.com/adjarar/webui-init.git
-cd webui-init
+git clone https://github.com/adjarar/webui-invoke-init.git
+cd webui-invoke-init
 chmod +x init.sh
 
 bash init.sh
